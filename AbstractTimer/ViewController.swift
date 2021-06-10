9c6b00691b646cbe281a063abf0360081d58d8e5
Sofia Chevrolat <sofia.chevrolat@madimedia.pro>
2021-06-10 15:53:16 -0400

    Initial Commit

---
+++ AbstractTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AbstractTimer
+//
+//  Created by Sofia Chevrolat on 10/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
